3c8b2b61ab564e2bc6d9bebb04a8877f0d70347f
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-03 16:51:58 +0530

    Added login controller with its unit test

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { createUser, getUserByEmail } from "../services/user";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "../utils/variables";
 
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
     try {
-
         const { name, email, password } = req.body;
 
         let user = await getUserByEmail(email);
@@ -27,6 +28,42 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
 
         return res.status(500).json({ 
             message: 'Failed to sign up',
+            error,
+            success: false,
+         });
+    }
+};
+
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+
+        let user = await getUserByEmail(email);
+        if (!user) {
+            throw Error('User not exists!');
+        }
+
+        const passwordVerified = await compare(password, user.password);
+        if(!passwordVerified) {
+            throw Error('Wrong password!');
+        }
+
+        const token = await sign({
+            userId: user.id,
+        }, JWT_SECRET);
+
+        res.status(201).json({
+            message: 'Login successfully', 
+            data: token,
+            success: true,
+        });
+
+    } catch (error) {
+        console.error('Error signing up:', error);
+
+        return res.status(500).json({ 
+            message: 'Failed to login',
             error,
             success: false,
          });
--- src/tests/controllers/auth.test.ts
+++ src/tests/controllers/auth.test.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { createUser, getUserByEmail } from "../../services/user";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { mockUser } from "../mock.data";
-import { signUp } from "../../controllers/auth";
+import { login, signUp } from "../../controllers/auth";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "../../utils/variables";
 
 
 
@@ -13,6 +15,11 @@ jest.mock('../../services/user', () => ({
 
 jest.mock('bcrypt', () => ({
     hash: jest.fn(),
+    compare: jest.fn(),
+}));
+
+jest.mock('jsonwebtoken', () => ({
+    sign: jest.fn(),
 }));
 
 let req: Partial<Request>;
@@ -66,8 +73,8 @@ describe('User controller', () => {
             await signUp(req as Request, res as Response, next);
 
             expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
-            expect(hash).not.toHaveBeenCalledWith();
-            expect(createUser).not.toHaveBeenCalledWith();
+            expect(hash).not.toHaveBeenCalled();
+            expect(createUser).not.toHaveBeenCalled();
             expect(res.status).toHaveBeenCalledWith(500);
             expect(res.json).toHaveBeenCalledWith({ 
                 message: 'Failed to sign up',
@@ -94,6 +101,75 @@ describe('User controller', () => {
                 error: new Error('Database Error'),
                 success: false,
              });
+        });
+    });
+
+
+
+    describe('login', () => {
+        
+        req = { body: { email: 'test@example.com', password: 'password' }};
+
+        const mockJwt = 'jdlsdnldswebfwjbej';
+
+
+        it('Should verify user and send jwt token', async () => {
+
+            (getUserByEmail as jest.Mock).mockResolvedValue(mockUser);
+            (compare as jest.Mock).mockResolvedValue(true);
+            (sign as jest.Mock).mockResolvedValue(mockJwt);
+
+            await login(req as Request, res as Response, next);
+
+            expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
+            expect(compare).toHaveBeenCalledWith(req.body.password, mockUser.password);
+            expect(sign).toHaveBeenCalledWith({ userId: mockUser.id }, JWT_SECRET);
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Login successfully', 
+                data: mockJwt,
+                success: true,
+            });
+        });
+
+
+        it('Should return fail responce if user not exists', async () => {
+
+            (getUserByEmail as jest.Mock).mockResolvedValue(null);
+            (compare as jest.Mock).mockResolvedValue(true);
+            (sign as jest.Mock).mockResolvedValue(mockJwt);
+
+            await login(req as Request, res as Response, next);
+
+            expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
+            expect(compare).not.toHaveBeenCalled();
+            expect(sign).not.toHaveBeenCalled();
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Failed to login',
+                error: new Error('User not exists!'),
+                success: false,
+            });
+        });
+
+
+        it('Should return fail responce if password is wrong', async () => {
+
+            (getUserByEmail as jest.Mock).mockResolvedValue(mockUser);
+            (compare as jest.Mock).mockResolvedValue(false);
+            (sign as jest.Mock).mockResolvedValue(mockJwt);
+
+            await login(req as Request, res as Response, next);
+
+            expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
+            expect(compare).toHaveBeenCalledWith(req.body.password, mockUser.password);
+            expect(sign).not.toHaveBeenCalled();
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Failed to login',
+                error: new Error('Wrong password!'),
+                success: false,
+            });
         });
     });
 });
