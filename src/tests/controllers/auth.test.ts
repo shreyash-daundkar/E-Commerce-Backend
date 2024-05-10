66a37fcbdcf9b279ccff750e2709c70e015dfa30
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-10 16:52:41 +0530

    update auth controllers tests

--- src/tests/controllers/auth.test.ts
+++ src/tests/controllers/auth.test.ts
@@ -5,6 +5,8 @@ import { mockUser } from "../mock.data";
 import { login, signUp } from "../../controllers/auth";
 import { sign } from "jsonwebtoken";
 import { JWT_SECRET } from "../../utils/variables";
+import { BadRequestException, InternalException, NotFoundException } from "../../errors/exceptions";
+import { ErrorCode } from "../../errors/error-codes";
 
 
 
@@ -70,17 +72,18 @@ describe('User controller', () => {
             (hash as jest.Mock).mockResolvedValue('password');
             (createUser as jest.Mock).mockResolvedValue(mockUser);
 
+            const badRequestException = new BadRequestException(
+                'User already exists!', 
+                ErrorCode.USER_ALREADY_EXIST, 
+                null,
+            )
+
             await signUp(req as Request, res as Response, next);
 
             expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
+            expect(next).toHaveBeenCalledWith(badRequestException);
             expect(hash).not.toHaveBeenCalled();
             expect(createUser).not.toHaveBeenCalled();
-            expect(res.status).toHaveBeenCalledWith(500);
-            expect(res.json).toHaveBeenCalledWith({ 
-                message: 'Failed to sign up',
-                error: new Error('User already exists!'),
-                success: false,
-             });
         });
 
 
@@ -90,17 +93,18 @@ describe('User controller', () => {
             (hash as jest.Mock).mockResolvedValue('password');
             (createUser as jest.Mock).mockRejectedValue(new Error('Database Error'));
 
+            const internalException = new InternalException(
+                'Failed to signup', 
+                ErrorCode.INTERNAL_EXCEPTION, 
+                new Error('Database Error'),
+            )
+
             await signUp(req as Request, res as Response, next);
 
             expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
             expect(hash).toHaveBeenCalledWith(req.body.password, 10);
             expect(createUser).toHaveBeenCalledWith(req.body);
-            expect(res.status).toHaveBeenCalledWith(500);
-            expect(res.json).toHaveBeenCalledWith({ 
-                message: 'Failed to sign up',
-                error: new Error('Database Error'),
-                success: false,
-             });
+            expect(next).toHaveBeenCalledWith(internalException);
         });
     });
 
@@ -139,17 +143,18 @@ describe('User controller', () => {
             (compare as jest.Mock).mockResolvedValue(true);
             (sign as jest.Mock).mockResolvedValue(mockJwt);
 
+            const notFoundException = new NotFoundException(
+                'User not exists!', 
+                ErrorCode.USER_NOT_FOUND, 
+                null
+            );
+
             await login(req as Request, res as Response, next);
 
             expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
+            expect(next).toHaveBeenCalledWith(notFoundException);
             expect(compare).not.toHaveBeenCalled();
             expect(sign).not.toHaveBeenCalled();
-            expect(res.status).toHaveBeenCalledWith(500);
-            expect(res.json).toHaveBeenCalledWith({
-                message: 'Failed to login',
-                error: new Error('User not exists!'),
-                success: false,
-            });
         });
 
 
@@ -159,17 +164,18 @@ describe('User controller', () => {
             (compare as jest.Mock).mockResolvedValue(false);
             (sign as jest.Mock).mockResolvedValue(mockJwt);
 
+            const badRequestException = new BadRequestException(
+                'Wrong password!', 
+                ErrorCode.INCORRECT_PASSWORD, 
+                null,
+            )
+
             await login(req as Request, res as Response, next);
 
             expect(getUserByEmail).toHaveBeenCalledWith(req.body.email);
             expect(compare).toHaveBeenCalledWith(req.body.password, mockUser.password);
+            expect(next).toHaveBeenCalledWith(badRequestException);
             expect(sign).not.toHaveBeenCalled();
-            expect(res.status).toHaveBeenCalledWith(500);
-            expect(res.json).toHaveBeenCalledWith({
-                message: 'Failed to login',
-                error: new Error('Wrong password!'),
-                success: false,
-            });
         });
-    });
+   });
 });
