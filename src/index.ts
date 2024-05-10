faeef17eaaf9d58a37e258cae0ad6733e4bb3954
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-10 16:49:49 +0530

    Added error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Express } from 'express';
 import { PORT } from './utils/variables';
 import indexRouter from './routes';
+import { errorMiddleware } from './middlewares/error';
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use('/api', indexRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
---
+++ src/middlewares/error.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpException } from "../errors/exceptions";
+
+export const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+    try {
+        
+        console.log(error.message, error.error);
+
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: error.error,
+            errorCode: error.errorCode,
+            success: false,
+        });
+
+    } catch (error) {
+        console.log('Error in error middleware', error);
+
+        res.status(500).json({
+            message: 'Error in error middleware',
+            error: error,
+            success: false,
+        });
+    }
+}
