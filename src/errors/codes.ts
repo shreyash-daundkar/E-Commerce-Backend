d691b73503dde78dc84b52e1f8abf5efa192acb7
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-11 19:35:00 +0530

    Added error codes

---
+++ src/errors/codes.ts
@@ -0,0 +1,21 @@
+export enum ErrorCode {
+
+    // Bad Request
+    USER_ALREADY_EXIST = 1001,
+    INCORRECT_PASSWORD = 1003,
+
+    // Not Found
+    USER_NOT_FOUND = 2001,
+    PRODUCT_NOT_FOUND = 2002,
+
+    // Internal
+    INTERNAL_EXCEPTION = 3001,
+
+    // Unprocessable
+    UNPROCESSABLEENTITY = 4001,
+
+    // Unauthorized
+    TOKEN_NOT_FOUND = 5001,
+    TOKEN_INVAID = 5002,
+    WRONG_TOKEN = 5003,
+}
