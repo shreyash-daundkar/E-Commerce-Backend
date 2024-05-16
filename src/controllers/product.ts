1df09455075e4a9988d125c58ce01b6e8d519c39
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-16 22:21:02 +0530

    Added get all products controller with its unit testing and routing.

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { readProductById } from "../services/product";
+import { getProductCount, readAllProducts, readProductById } from "../services/product";
 import { InternalException } from "../errors/exceptions";
 
 export const getProductById = async (req: Request, res: Response, next: NextFunction) => {
@@ -13,6 +13,36 @@ export const getProductById = async (req: Request, res: Response, next: NextFunc
         return res.status(200).json({
             message: 'Get Product successfully', 
             data: product,
+            success: true,
+        });
+        
+    } catch (error) {
+        return next( new InternalException(error));
+    }
+}
+
+
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { skip, take } = req.query;
+
+        const products = await readAllProducts(
+            skip ? +skip : 0, 
+            take ? +take : 5
+        );
+
+        if(!products) throw new Error('Error reading products');
+
+        const count = await getProductCount();
+
+        if(count === null) {
+            throw new Error('Error counting products');
+        }
+    
+        return res.status(200).json({
+            message: 'Get Products successfully', 
+            data: products,
+            count,
             success: true,
         });
         
--- src/tests/controllers/product.test.ts
+++ src/tests/controllers/product.test.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import { readProductById } from "../../services/product";
+import { getProductCount, readAllProducts, readProductById } from "../../services/product";
 import { mockProduct } from "../mock.data";
-import { getProductById } from "../../controllers/product";
+import { getAllProducts, getProductById } from "../../controllers/product";
 import { InternalException } from "../../errors/exceptions";
+import { skip } from "node:test";
 
 jest.mock('../../services/product', () => ({
     readProductById: jest.fn(),
+    readAllProducts: jest.fn(),
+    getProductCount: jest.fn(),
 }));
 
 let req: Partial<Request>;
@@ -62,5 +65,58 @@ describe('Product controller', () => {
             expect(readProductById).toHaveBeenCalledWith(1);
             expect(next).toHaveBeenCalledWith(internalException);
         });
-    });   
+    });
+    
+    
+    describe('get all Products', () => {
+
+        beforeEach(() => {
+            req = { 
+                query: {
+                    skip: '0',
+                    take:'5',
+                }
+            }
+        })
+        
+        it('should get products and return success responce', async () => {
+        
+            (readAllProducts as jest.Mock).mockResolvedValue([mockProduct]);
+            (getProductCount as jest.Mock).mockResolvedValue(1);
+        
+            await getAllProducts(req as Request, res as Response, next);
+
+            const { skip, take } = req.query!;
+
+            
+            expect(readAllProducts).toHaveBeenCalledWith(+skip!, +take!);
+            expect(getProductCount).toHaveBeenCalled()
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Get Products successfully', 
+                data: [mockProduct],
+                count: 1,
+                success: true,
+            });
+        
+        });
+        
+        
+        it('should return internal exception if error getting product', async () => {
+        
+            (readAllProducts as jest.Mock).mockResolvedValue(null);
+            (getProductCount as jest.Mock).mockResolvedValue(1);
+        
+            const error = new Error('Error deleting product');
+            const internalException = new InternalException(error);
+        
+            await getAllProducts(req as Request, res as Response, next);
+
+            const { skip, take } = req.query!;
+            
+            expect(readAllProducts).toHaveBeenCalledWith(+skip!, +take!);
+            expect(getProductCount).not.toHaveBeenCalled();
+            expect(next).toHaveBeenCalledWith(internalException);
+        });
+    });
 });
