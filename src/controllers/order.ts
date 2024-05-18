734fa6a4a5f75bf3cf4fafc8701224e1e9ec2b8f
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-18 17:47:46 +0530

    Added Make order controller with its unit testing and routing.

---
+++ src/controllers/order.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { getCartItemsByUserId } from "../services/cart";
+import { User } from "../services/prisma";
+import { InternalException, NotFoundException } from "../errors/exceptions";
+import { ErrorCode } from "../errors/codes";
+import { createOrder, getOrders, updateOrder } from "../services/order";
+
+
+
+export const makeOrder = async (req: Request, res: Response, next: NextFunction) => {
+    try { 
+        const user: User = req.body.user;
+
+        const cartItems = await getCartItemsByUserId(user.id);
+        if(!cartItems) {
+            throw new Error('Error getting cart items');
+        }
+
+        if(cartItems.length === 0) {
+            return next(
+                new NotFoundException(
+                    'Not found cart items',
+                    ErrorCode.CART_ITEM_NOT_FOUND,
+                    null,
+                ),
+            );
+        }
+
+        let amount = 0;
+        const products = cartItems.map(item => {
+            amount += item.quantity * item.product.price
+            return {
+                productId: item.productId,
+                quantity: item.quantity,
+            }
+        });
+
+        const order = await createOrder({
+            userId: user.id,
+            amount,
+            products,
+        });
+        if(!order) {
+            throw new Error('Error creating order');
+        }
+    
+        return res.status(201).json({
+            message: 'Maked order successfully', 
+            data: order,
+            success: true,
+        });
+        
+    } catch (error) {
+        return next( new InternalException(error));
+    }
+}
+
+
---
+++ src/tests/controllers/order.test.ts
@@ -0,0 +1,99 @@
+import { Request, Response, NextFunction } from "express";
+import { getCartItemsByUserId } from "../../services/cart";
+import { createOrderInputMock, createUserInputMock, mockCartItem, mockCartItemWithProduct, mockOrder } from "../mock.data";
+import { createOrder } from "../../services/order";
+import { makeOrder } from "../../controllers/order";
+import { InternalException, NotFoundException } from "../../errors/exceptions";
+import { ErrorCode } from "../../errors/codes";
+
+jest.mock('../../services/cart', () => ({
+    getCartItemsByUserId: jest.fn(),
+}));
+
+jest.mock('../../services/order', () => ({
+    createOrder: jest.fn(),
+}));
+
+let req: Partial<Request>;
+let res: Partial<Response>;
+let next: jest.MockedFunction<NextFunction>;
+
+
+
+describe('Order controller', () => {
+    
+    beforeEach(() => {
+        res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+        next = jest.fn();
+    });
+    
+    afterEach(() => jest.clearAllMocks());
+
+
+
+    describe('Create Order', () => {
+    
+        beforeEach(() => {
+            req = { 
+                body: {
+                    user: {
+                        id: 1,
+                    },
+                },
+            }
+        });
+        
+    
+        it('should fetch cart, make order and return success responce', async () => {
+    
+            (getCartItemsByUserId as jest.Mock).mockResolvedValue([mockCartItemWithProduct]);
+            (createOrder as jest.Mock).mockResolvedValue(mockOrder);
+            
+            await makeOrder(req as Request, res as Response, next);
+            
+            expect(getCartItemsByUserId).toHaveBeenCalledWith(1);
+            expect(createOrder).toHaveBeenCalledWith(createOrderInputMock);
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Maked order successfully', 
+                data: mockOrder,
+                success: true,
+            });
+    
+        });
+
+        it('should called next with not found exception if cart is empty', async () => {
+    
+            (getCartItemsByUserId as jest.Mock).mockResolvedValue([]);
+            (createOrder as jest.Mock).mockResolvedValue(mockOrder);
+    
+            const notFoundException = new NotFoundException(
+                'Not found cart items',
+                ErrorCode.CART_ITEM_NOT_FOUND,
+                null,
+            );
+    
+            await makeOrder(req as Request, res as Response, next);
+            
+            expect(getCartItemsByUserId).toHaveBeenCalledWith(1);
+            expect(createOrder).not.toHaveBeenCalled();
+            expect(next).toHaveBeenCalledWith(notFoundException);
+        });
+    
+    
+        it('should return internal exception if error getting cart', async () => {
+    
+            (getCartItemsByUserId as jest.Mock).mockResolvedValue(null);
+            (createOrder as jest.Mock).mockResolvedValue(mockOrder);
+    
+            const error = new Error('Error getting cart items');
+            const internalException = new InternalException(error);
+    
+            await makeOrder(req as Request, res as Response, next);
+            
+            expect(getCartItemsByUserId).toHaveBeenCalledWith(1);
+            expect(createOrder).not.toHaveBeenCalled();
+            expect(next).toHaveBeenCalledWith(internalException);
+        });
+    });
+});
