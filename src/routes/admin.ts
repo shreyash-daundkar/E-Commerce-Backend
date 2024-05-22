d3386583dd0cdfd89c1311e8b152bd191df74ad7
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-22 16:33:31 +0530

    Updated admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { errorHandler } from '../errors/handler';
-import { addProduct, editProduct } from '../controllers/admin';
+import { addProduct, completeOrder, editProduct, getAllActiveOrders, removeProduct } from '../controllers/admin';
 
 const adminRouter: Router = Router();
 
 adminRouter.post('/', addProduct);
-adminRouter.put('/', editProduct);
+adminRouter.get('/', getAllActiveOrders);
+adminRouter.post('/:id', completeOrder);
+adminRouter.put('/:id', editProduct);
+adminRouter.delete('/:id', removeProduct);
 
 export default adminRouter;
