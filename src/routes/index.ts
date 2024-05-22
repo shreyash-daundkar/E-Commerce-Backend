688f156e6ab77673dd07c107a90c881754426b9e
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-22 16:34:16 +0530

    Fixed routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import authRouter from './auth';
-import userRouter from './user';
-import productRouter from './product';
 import authMiddleware from '../middlewares/auth';
+import adminRouter from './admin';
+import adminMiddleware from '../middlewares/admin';
+import productRouter from './product';
+import cartRouter from './cart';
+import orderRouter from './order';
 
 const indexRouter: Router = Router();
 
 indexRouter.use('/auth', authRouter);
-indexRouter.use('/user', authMiddleware, userRouter);
+indexRouter.use('/admin', authMiddleware, adminMiddleware, adminRouter);
 indexRouter.use('/product', authMiddleware, productRouter);
+indexRouter.use('/cart', authMiddleware, cartRouter);
+indexRouter.use('/order', authMiddleware, orderRouter);
 
 export default indexRouter;
