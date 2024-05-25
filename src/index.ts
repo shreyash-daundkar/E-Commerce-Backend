dac7abfc3f0b65f60ceb281f7722b8dadcbec1b8
Shreyash Daundkar <productiveappa@gmail.com>
2024-05-25 22:35:21 +0530

    Log in access.log using morgan

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import express, { Express } from 'express';
 import { PORT } from './utils/variables';
 import indexRouter from './routes';
 import errorMiddleware from './middlewares/error';
+import path from 'path';
+import fs from 'fs';
+import morgan from 'morgan'
 
 const app: Express = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
 
